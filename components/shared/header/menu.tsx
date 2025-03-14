38194ee9a999e96ccc68b5dd22195b63586c2481
Alex6y6 <alshyfamjdd@gmail.com>
2025-03-14 16:17:18 +0000

    26-add-theme-color

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,14 +1,47 @@
+import { EllipsisVertical } from 'lucide-react'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
+import ThemeSwitcher from './theme-switcher'
 import CartButton from './cart-button'
 import UserButton from './user-button'
 
-export default function Menu() {
+const Menu = ({ forAdmin = false }: { forAdmin?: boolean }) => {
   return (
     <div className='flex justify-end'>
-      <nav className='flex gap-3 w-full'>
+      {/* Desktop and tablet menu */}
+      <nav className='md:flex gap-3 w-full hidden'>
+        <ThemeSwitcher />
         <UserButton />
+        {forAdmin ? null : <CartButton />}
+      </nav>
 
-        <CartButton />
+      {/* Mobile menu */}
+      <nav className='md:hidden flex gap-3 w-full'>
+        <UserButton />
+        {forAdmin ? null : <CartButton />}
+        <Sheet>
+          <SheetTrigger className='align-middle header-button ml-auto'>
+            <EllipsisVertical className='h-6 w-6' />
+          </SheetTrigger>
+          <SheetContent className='bg-black text-white flex flex-col items-start'>
+            <SheetHeader className='w-full'>
+              <div className='flex items-center justify-between'>
+                <SheetTitle>Site Menu</SheetTitle>
+                <SheetDescription></SheetDescription>
+              </div>
+            </SheetHeader>
+            <ThemeSwitcher />
+          </SheetContent>
+        </Sheet>
       </nav>
     </div>
   )
 }
+
+export default Menu
