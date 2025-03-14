fa51f65d15381720e64865a052c51944f834b1b2
Alex6y6 <alshyfamjdd@gmail.com>
2025-03-14 01:42:18 +0000

    17-create-checkout-page

--- lib/constants.ts
+++ lib/constants.ts
@@ -12,3 +12,43 @@ export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
 export const FREE_SHIPPING_MIN_PRICE = Number(
   process.env.FREE_SHIPPING_MIN_PRICE || 3
 )
+export const AVAILABLE_PAYMENT_METHODS = [
+  {
+    name: 'PayPal',
+    commission: 0,
+    isDefault: true,
+  },
+  {
+    name: 'Stripe',
+    commission: 0,
+    isDefault: false,
+  },
+  {
+    name: 'Cash On Delivery',
+    commission: 0,
+    isDefault: true,
+  },
+]
+export const DEFAULT_PAYMENT_METHOD =
+  process.env.DEFAULT_PAYMENT_METHOD || 'Cash On Delivery'
+
+export const AVAILABLE_DELIVERY_DATES = [
+  {
+    name: 'Tomorrow',
+    daysToDeliver: 1,
+    shippingPrice: 5,
+    freeShippingMinPrice: 0,
+  },
+  {
+    name: 'Next 3 Days',
+    daysToDeliver: 3,
+    shippingPrice: 4,
+    freeShippingMinPrice: 0,
+  },
+  {
+    name: 'Next 5 Days',
+    daysToDeliver: 5,
+    shippingPrice: 4,
+    freeShippingMinPrice: 100,
+  },
+]
